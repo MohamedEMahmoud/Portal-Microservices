9f1d9aac0434e4fe8f44ceb5537954bcf1415b4c
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2022-06-19 13:32:28 +0200

    Updates

--- common/src/middlewares/current-user.ts
+++ common/src/middlewares/current-user.ts
@@ -13,6 +13,11 @@ declare global {
   }
 }
 
+declare module 'express-session' {
+  interface SessionData {
+    jwt: string;
+  }
+}
 export const currentUser = (
   req: Request,
   _res: Response,
