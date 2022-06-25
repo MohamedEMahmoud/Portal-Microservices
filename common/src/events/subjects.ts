43843ae45c888b0ebfef40d039ceb06bc2c36406
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2022-06-25 17:26:31 +0200

    Updates

---
+++ common/src/events/subjects.ts
@@ -0,0 +1,8 @@
+export enum Subjects {
+  UserCreated = 'user:created',
+  UserUpdated = 'user:updated',
+  UserDeleted = 'user:deleted',
+  ProductCreated = 'product:created',
+  ProductUpdated = 'product:updated',
+  ProductDeleted = 'product:deleted',
+}
